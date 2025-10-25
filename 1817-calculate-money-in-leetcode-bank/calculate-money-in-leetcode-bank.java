51f0a9170815b01b53f0fb158a9e0bcdb71a9d73
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-10-25 13:17:21 +0530

    Time: 1 ms (56.85%) | Memory: 40.7 MB (32.47%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -2,9 +2,9 @@ class Solution {
     public int totalMoney(int n) {
         int sum=0;
         int count=0;
-        int start=1;
+        int start=0;
         while(count<n){
-        for(int i=0;i<7 && count<n;i++){
+        for(int i=1;i<=7 && count<n;i++){
             sum+=start+i;
             count++; 
         }
