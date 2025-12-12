4bd8011f5ae76079de19865ce5e4f33bd26844e5
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-12-12 12:24:50 +0530

    Time: 13 ms (59.82%) | Memory: 42 MB (59.82%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countTriples(int n) {
+        int count=0;
+     for(int a=1;a<n;a++){
+        for(int b=1;b<n;b++){
+            int sum=a*a+b*b;
+            int c=(int)Math.sqrt(sum);
+            if(c<=n && c*c==sum){
+                count++;
+            }
+        }
+     }   
+     return count;
+    }
+}
